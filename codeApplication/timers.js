385ea9d3ecd29ab4ee96c2e72b2edc800ab0d0df
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-02-04 23:38:26 +0200

    Update timers.js

--- codeApplication/timers.js
+++ codeApplication/timers.js
@@ -1,11 +1,17 @@
 'use strict';
+// if we want to make a certain time delay
 
-const sleep = (msec) => {
-  const end = new Date().getTime() + msec;
-  while (new Date().getTime() < end);
+// As long as the cycle while "spins", nothing can happen in the program.
+const delay = (msec) => {
+	console.log(`  Wait about ${msec/1000} sec`);
+	const end = new Date().getTime() + msec;
+	while (new Date().getTime() < end);
 };
-console.log('Start sleep: ' + new Date().toISOString());
-console.log('  Sleep about 3 sec');
-sleep(3000);
-console.log('After sleep: ' + new Date().toISOString());
+console.log('Start delay: ' + new Date().toISOString());
+delay(5000);
+console.log('End of waiting: ' + new Date().toISOString());
+
+
+
+
 
