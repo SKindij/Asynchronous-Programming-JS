ff99d7454460c81b1812e4f266441e5762d4559a
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-02-05 00:57:40 +0200

    Update timers.js

--- codeApplication/timers.js
+++ codeApplication/timers.js
@@ -1,6 +1,7 @@
 'use strict';
 // if we want to make a certain time delay
 
+// EXAMPLE 1
 // As long as the cycle while "spins", nothing can happen in the program.
 const delay = (msec) => {
 	console.log(`  Wait about ${msec/1000} sec`);
@@ -11,7 +12,19 @@ console.log('Start delay: ' + new Date().toISOString());
 delay(5000);
 console.log('End of waiting: ' + new Date().toISOString());
 
-
-
-
+// EXAMPLES 2 & 3
+// the timer delays the resolution of the promise
+const delay = (msec) => new Promise((resolve) => {
+  console.log(`  Wait about ${msec/1000} sec`);
+  setTimeout(resolve, msec);
+});
+// we run the version with IIFE (Immediately Invoked Function Expression)
+(async () => {
+  console.log('Start delay: ' + new Date().toISOString());
+  await delay(5000);
+  console.log('End of waiting: ' + new Date().toISOString());
+})();
+// we run the version with .then()
+console.log('Start delay: ' + new Date().toISOString());
+delay(5000).then(() => { console.log('End of waiting: ' + new Date().toISOString()); });
 
